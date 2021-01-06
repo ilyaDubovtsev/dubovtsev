1d37b32d931131e001249234589ddbd1ed3c8803
dubovtsev <dubovtsev@skbkontur.ru>
2021-01-06 23:59:58 +0400

    Сконфигурировал Startup

--- dubovtsev/Startup.cs
+++ dubovtsev/Startup.cs
@@ -16,6 +16,7 @@ namespace dubovtsev
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,9 @@ namespace dubovtsev
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseMvc();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
